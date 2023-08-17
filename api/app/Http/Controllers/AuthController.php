0ce50cd129b878a2d32776fb28bcb86d6e6beb6a
kaldray <jordray@protonmail.com>
2023-08-17 13:46:25 +0200

    feat: create login route and request validation

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginStoreRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * @param LoginStoreRequest $request
+     * @return Response
+     */
+    public function login(LoginStoreRequest $request): Response
+    {
+        $credentials = $request->validated();
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            $user = Auth::user();
+            return response(
+                ["message" => "Utilisateur authentifié", "status" => 201, "user" => $user],
+                Response::HTTP_OK,
+            );
+        }
+        return response(["message" => "Les informations sont incorrectes.", "status" => 401], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard("web")->logout();
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+        return response(["message" => "Utilisateur déconnecter", "status" => 200], 200);
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
     return $request->user();
 });
+
+Route::controller(\App\Http\Controllers\AuthController::class)->group(function () {
+    Route::get("/login", "login");
+    Route::post("/logout", "logout");
+});
