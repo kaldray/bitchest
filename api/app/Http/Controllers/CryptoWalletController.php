7601bcb978a67353ea3a58cb0aee305575ae9b03
kaldray <jordray@protonmail.com>
2023-09-04 11:59:09 +0200

    refactor: use services on CryptoWalletController.php

--- api/app/Http/Controllers/CryptoWalletController.php
+++ api/app/Http/Controllers/CryptoWalletController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\StoreCryptoWalletRequest;
 use App\Models\CryptoWallet;
 use App\Models\CurrencyHistory;
 use App\Services\CryptoWalletServices;
+use App\Services\CurrencyHistoryServices;
 use Illuminate\Support\Facades\Response;
 
 class CryptoWalletController extends Controller
@@ -17,8 +18,10 @@ class CryptoWalletController extends Controller
      * Store a newly created resource in storage.
      */
 
-    public function __construct(protected CryptoWalletServices $cryptoWalletServices)
-    {
+    public function __construct(
+        protected CryptoWalletServices $cryptoWalletServices,
+        protected CurrencyHistoryServices $currencyHistoryServices,
+    ) {
     }
 
     public function store(StoreCryptoWalletRequest $request)
@@ -41,31 +44,14 @@ class CryptoWalletController extends Controller
     public function delete(CryptoWallet $cryptoWallet)
     {
         try {
-            $quotingForSell = CurrencyHistory::whereDate("date", now())
-                ->where("currency_id", "=", $cryptoWallet->currency_id)
-                ->firstOrFail();
-            $cryptoToDeleted = CryptoWallet::where("currency_id", "=", $cryptoWallet->currency_id)
-                ->where("user_id", "=", $cryptoWallet->user_id)
-                ->get()
-                ->each(function ($item) {
-                    $item->delete();
-                });
-            $ids = $cryptoToDeleted->map(function ($item) {
-                return $item->id;
-            });
-            $benef = CryptoSale::dispatch($quotingForSell, $cryptoToDeleted);
-            $diff = CryptoProfits::dispatch($cryptoToDeleted);
-            $longueur = count($benef[0]);
-            $capital_gain = [];
-            for ($i = 0; $i < $longueur; $i++) {
-                array_push($capital_gain, $benef[0][$i] - $diff[0][$i]);
-            }
-            $withTrashed = CryptoWallet::onlyTrashed()
-                ->whereIn("id", $ids)
-                ->get();
-            $withTrashed->each(function (CryptoWallet $item, $key) use ($capital_gain) {
-                $item->update(["capital_gain" => $capital_gain[$key]]);
-            });
+            $quotingForSell = $this->currencyHistoryServices->getQuotingAtCurrentDate(
+                $cryptoWallet->id,
+            );
+            $deletedCrypto = $this->cryptoWalletServices->deleteCrypto($cryptoWallet);
+            $benef = CryptoSale::dispatch($quotingForSell, $deletedCrypto);
+            $diff = CryptoProfits::dispatch($deletedCrypto);
+            $capital_gain = $this->cryptoWalletServices->calculateCapitalGain($benef, $diff);
+            $this->cryptoWalletServices->fillCapitalGainValue($deletedCrypto, $capital_gain);
             return Response::json(
                 [
                     "message" => "L'opération s'est déroulée avec succès",
--- api/app/Services/CryptoWalletServices.php
+++ api/app/Services/CryptoWalletServices.php
@@ -7,6 +7,7 @@ use App\Models\CryptoWallet;
 
 class CryptoWalletServices
 {
+    private array $capital_gain = [];
     public function __construct(protected CryptoWallet $cryptoWallet)
     {
     }
@@ -14,5 +15,34 @@ class CryptoWalletServices
     public function purchaseCurrency(StoreCryptoWalletRequest $request)
     {
         return $this->cryptoWallet::create($request->validated());
+    }
+
+    public function deleteCrypto(CryptoWallet $cryptoWallet)
+    {
+        return $this->cryptoWallet->sellCrypto($cryptoWallet);
+    }
+
+    public function getId($cryptoToDeleted)
+    {
+        return $cryptoToDeleted->map(function ($item) {
+            return $item->id;
+        });
+    }
+
+    public function calculateCapitalGain(array $benef, array $diff)
+    {
+        $longueur = count($benef[0]);
+        $capital_gain = [];
+        for ($i = 0; $i < $longueur; $i++) {
+            array_push($capital_gain, $benef[0][$i] - $diff[0][$i]);
+        }
+        return $capital_gain;
+    }
+
+    public function fillCapitalGainValue($deletedCrypto, $capital_gain)
+    {
+        $deletedCrypto->each(function (CryptoWallet $item, $key) use ($capital_gain) {
+            $item->update(["capital_gain" => $capital_gain[$key]]);
+        });
     }
 }
