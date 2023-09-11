b56356c64e0ab4fcbdf71f2ecf0c87f80a60b919
kaldray <jordray@protonmail.com>
2023-09-11 09:44:42 +0200

    feat: don't display devtools on prod

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,8 +1,19 @@
 import { RouterProvider } from "@tanstack/react-router";
 import { ChakraProvider } from "@chakra-ui/react";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 import { router } from "@/router";
+import React from "react";
+
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : React.lazy(() =>
+      // Lazy load in development
+      import("@tanstack/router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel
+      })),
+    );
 
 function App() {
   return (
