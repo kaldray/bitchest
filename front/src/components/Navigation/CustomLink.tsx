67cb38bda8e2d12a36a770b431b4c86253648f5d
kaldray <jordray@protonmail.com>
2024-03-17 13:46:38 +0100

    fix: ts-error

--- front/src/components/Navigation/CustomLink.tsx
+++ front/src/components/Navigation/CustomLink.tsx
@@ -1,17 +1,10 @@
 import { Link as ChakraLink, type ChakraComponent, type ComponentWithAs } from "@chakra-ui/react";
-import { Link, type ToSubOptions, type UseLinkPropsOptions } from "@tanstack/react-router";
+import { Link } from "@tanstack/react-router";
+
 import { useSidebarStore } from "@/store/sidebarStore";
-import type { LinkProps, ParamOptions } from "@tanstack/react-router";
+import type { LinkProps } from "@tanstack/react-router";
 
 interface CustomLinkProps extends ChakraComponent<ComponentWithAs<"a">, LinkProps> {}
-
-import { chakra } from "@chakra-ui/react";
-
-// const MagicLink = chakra<typeof Link, LinkProps>(Link, {
-//   // ensure that you're forwarding all of the required props for your case
-//   shouldForwardProp: (prop) => ["href", "target", "children"].includes(prop),
-// });
-
 
 export const CustomLink: CustomLinkProps = ({ children, to, params, ...style }) => {
   const closeSidebar = useSidebarStore((state) => state.closeSideBar);
@@ -23,9 +16,11 @@ export const CustomLink: CustomLinkProps = ({ children, to, params, ...style }) 
 
   return (
     <>
+      {/*//@ts-ignore*/}
       <ChakraLink
         activeProps={{
           style: {
+            //@ts-ignore
             fontWeight: "bold",
             textDecoration: "underline",
           },
